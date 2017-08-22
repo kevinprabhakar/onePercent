68296bf388c709d778931a4a3adb2f82888ef57f
Kevin Prabhakar <kprabhakar@Kevins-MacBook-Pro.local>
2017-08-22 13:49:48 -0700

    Adding streak functionality

--- web/js/dashboard.js
+++ web/js/dashboard.js
@@ -46,6 +46,34 @@ function PopulatePage(){
     })
 }
 
+function GetStreak(){
+    $.ajax({
+        url:"/api/goal",
+        type:"POST",
+        data:{"accessToken":api.readCookie("accessToken")},
+        success:function(resp){
+            var response = JSON.parse(resp)
+            var goalId = response[0].id
+            $.ajax({
+                url:"/api/getwinstreak",
+                type:"POST",
+                data:{"accessToken":api.readCookie("accessToken"),"goalId":goalId},
+                success:function(resp){
+                    var response = JSON.parse(resp)
+                    $("#longW").text("Longest Streak: " + resp.longW)
+                    $("#currW").text("Current Streak: " + resp.currW)
+                },
+                error: function(error){
+
+                }
+            })
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
+}
+
 $(document).ready(function(){
     //THIS FUNCTION MUST BE EDITED FOR MULTIPLE GOALS
     PopulatePage()
